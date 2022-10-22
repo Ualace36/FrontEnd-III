27b8c6823ba782dca9add1dcfa5d259bcea4bf2b
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-10-22 06:54:34 -0300

    Este é o primeiro commit referente o conteúdo apresentado na segunda aula

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { SegundaAula } from "./aulas/SegundaAula"
+import { LoginPage } from "./pages/Login"
+
+
+function App() {
+
+  return (
+  //  <LoginPage></LoginPage>
+   <SegundaAula></SegundaAula>
+   
+  )
+}
+
+export default App
---
+++ src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,17 @@
+import './style.scss'
+
+export function SegundaAula(){
+    return(
+        <main>
+        <h1>Segunda Aula</h1>
+        <h2>Projeto Escolhido</h2>
+        <ul>
+            <li>
+                <h1>Primeiro Componente</h1>
+                <p>A Buffer é um aplicativo de software projetado para gerenciar contas em redes sociais</p>
+                <p>A Linear permite gerenciar o desenvolvimento de software e rastrear bugs</p>
+            </li>
+        </ul>
+        </main>
+    )
+}
