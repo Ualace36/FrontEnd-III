0832f56a7acdb4c96e30b319475100b7b3605d4e
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-10-24 03:23:58 -0300

    Utilização do map para a inserção de cards em uma página

--- src/aulas/TerceiraAula/index.jsx
+++ src/aulas/TerceiraAula/index.jsx
@@ -1,6 +1,8 @@
 import './style.scss'
 const title = "Title Card 25"
-const components = [{name:'component card', description: 'descrição do card', imagem:''} ]
+const components = [{name:'component card', description: 'descrição do card', imagem:'https://media.tenor.com/xaJrTrfiRcEAAAAM/happy-panda-bamboo.gif'},
+{name:'component card2', description: 'descrição do card2', imagem:'https://media.tenor.com/pF3s48bhdIsAAAAM/marin-kitagawa-anime-shy.gif'},
+{name:'component card3', description: 'descrição do card3', imagem:'https://i.pinimg.com/originals/de/67/22/de67224d07c99e9357d6bc4f17465761.gif'} ]
 
 export function TerceiraAula(){
     return(
@@ -8,12 +10,17 @@ export function TerceiraAula(){
         <div>
             <h1>Lista de components de um sistema</h1>
                 <ul>
-                    <li>
-                        <img src=""/>
-                        <h1>{components[0].name}</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente explicabo temporibus nam fugiat, odio commodi cumque sequi, illo ipsum consectetur unde delectus, minima eos ipsam ex repudiandae distinctio eveniet mollitia?</p>
+                {
+                     components.map(component => (
+                        <li>
+
+                        <img src={component.imagem}/>
+                        <h1>{component.name}</h1>
+                        <p>{component.description}</p>
                     </li>
                     
+                     ))       
+                        }
                 </ul>
             
         </div>
